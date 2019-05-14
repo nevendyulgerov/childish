64bea5e5e95bdb3a87b4e5f795e504f99e9e78dd
Neven Dyulgerov <neven.diulgerov@gmail.com>
2019-05-14 14:14:14 +0300

    Remove obsolete input props

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,20 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 const Input = (props) => {
-    const { type, value, name, id, placeholder, title, className, autoFocus, active, readonly, valid, validated, loading, autoComplete, disabled, required, onChange, onClick, onKeyDown, onFocus, onBlur } = props;
+    const { type, value, name, id, placeholder, title, autoFocus, autoComplete, disabled, required, onChange, onClick, onKeyDown, onFocus, onBlur } = props;
     let inputProps = {};
-
-    const inputClass = classNames({
-        'form-control__input': true,
-        'form-control--active': active,
-        'form-control__input--readonly': readonly,
-        'form-control__input--valid': validated && valid,
-        'form-control__input--invalid': validated && !valid,
-        'form-control__input--loading': loading,
-        [className]: className !== ''
-    });
 
     if (name !== '') {
         inputProps.name = name;
@@ -50,7 +39,6 @@ const Input = (props) => {
             disabled={disabled}
             required={required}
             autoFocus={autoFocus}
-            className={inputClass}
             onChange={change}
             onClick={click}
             onKeyDown={keyDown}
@@ -79,39 +67,14 @@ Input.propTypes = {
     ]).isRequired,
     name: PropTypes.string,
     id: PropTypes.string,
-    label: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.node
-    ]),
     placeholder: PropTypes.string,
     title: PropTypes.string,
-    size: PropTypes.oneOf([
-        'sm',
-        'md',
-        'lg'
-    ]),
-    className: PropTypes.string,
-    controlClassName: PropTypes.string,
-    groupClassName: PropTypes.string,
-    labelClassName: PropTypes.string,
-    icon: PropTypes.string,
-    customProps: PropTypes.shape({
-        [PropTypes.string]: PropTypes.oneOfType([
-            PropTypes.string,
-            PropTypes.number
-        ])
-    }),
     autoComplete: PropTypes.string,
     autoFocus: PropTypes.bool,
     active: PropTypes.bool,
     readonly: PropTypes.bool,
-    validated: PropTypes.bool,
-    valid: PropTypes.bool,
-    loading: PropTypes.bool,
     disabled: PropTypes.bool,
     required: PropTypes.bool,
-    prependIcon: PropTypes.bool,
-    inline: PropTypes.bool,
     onChange: PropTypes.func,
     onClick: PropTypes.func,
     onKeyDown: PropTypes.func,
@@ -123,27 +86,14 @@ Input.defaultProps = {
     type: 'text',
     name: '',
     id: '',
-    label: '',
     placeholder: '',
     title: '',
-    size: 'md',
-    className: '',
-    controlClassName: '',
-    groupClassName: '',
-    labelClassName: '',
-    icon: '',
-    customProps: {},
     autoComplete: '',
     autoFocus: false,
     active: false,
     readonly: false,
-    validated: false,
-    valid: true,
-    loading: false,
     disabled: false,
     required: false,
-    prependIcon: true,
-    inline: true,
     onChange: () => {},
     onClick: () => {},
     onKeyDown: () => {},
